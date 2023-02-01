be4ad84137a51f4529df34977a5dfb13b622d5ba
AyushAwasthi1999 <ayushawasthi504@gmail.com>
2023-02-01 14:45:25 +0530

    cppassignment

---
+++ MODULE_4.1_(C++Basic)/OOP.cpp
@@ -0,0 +1,13 @@
+// What is OOP?
+/*
+ANSWER :- object-oriented programming
+        Object-oriented programming (OOP) is a computer programming model that organizes
+        software design around data, or objects, rather than functions and logic. An object can be
+        defined as a data field that has unique attributes and behavior. 
+
+        OOP focuses on the objects that developers want to manipulate rather than the logic
+        required to manipulate them. This approach to programming is well-suited for programs that
+        are large, complex and actively updated or maintained. This includes programs for
+        manufacturing and design, as well as mobile applications; for example, OOP can be used for
+        manufacturing system simulation software.         
+*/
---
+++ MODULE_4.1_(C++Basic)/listoopconcepts.cpp
@@ -0,0 +1,15 @@
+// List OOP concepts
+
+/*
+    Basic Object-Oriented Programming (OOPS) Concept in C++
+
+    01:- Classes
+    02:- Objects
+    03:- Abstraction
+    04:- Encapsulation
+    05:- Inheritance
+    06:- Polymorphism
+    07:- Dynamic Binding
+    08:- Message Passing
+
+*/
